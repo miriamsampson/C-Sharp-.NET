a1495680b2582ab5833918b6153aea2fae421487
miriamsampson <sumayahsampson@gmail.com>
2025-08-14 15:14:08 -0400

    Final Assignment

---
+++ Program(StudentEFApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var context = new SchoolContext())
+        {
+            var student = new Student() { StudentName = "John Doe" };
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully!");
+        }
+
+        Console.ReadLine();
+    }
+}
+
